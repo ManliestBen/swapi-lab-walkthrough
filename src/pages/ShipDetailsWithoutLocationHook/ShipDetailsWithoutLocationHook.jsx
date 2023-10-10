3dabb1e6ee0eb3dd089803dbea3e6f6132186470
Ben Manley <benjamintmanley@gmail.com>
2023-10-09 20:06:49 -0500

    add code for component WITHOUT useLocation hook

--- src/pages/ShipDetailsWithoutLocationHook/ShipDetailsWithoutLocationHook.jsx
+++ src/pages/ShipDetailsWithoutLocationHook/ShipDetailsWithoutLocationHook.jsx
@@ -1,8 +1,33 @@
+import { useParams, Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import * as apiService from '../../services/apiService'
+import styles from './ShipDetailsWithoutLocationHook.module.css'
+
 const ShipDetailsWithoutLocationHook = () => {
+  const {shipId} = useParams()
+  const [shipDetails, setShipDetails] = useState({})
+
+  useEffect(() => {
+    const fetchShipDetails = async () => {
+      const shipData = await apiService.getShipData(shipId)
+      setShipDetails(shipData)
+    }
+    fetchShipDetails()
+  })
+
+
+  if (!shipDetails.name) return <h2>Please wait... Loading Ship...</h2>
+
   return (
-    <>
-      <h1>ShipDetailsWithoutLocationHook</h1>
-    </>
+
+    <div className={styles.shipDetails}>
+      <h2>NAME: {shipDetails.name}</h2>
+      <h2>MODEL: {shipDetails.model}</h2>
+      <h3>MADE BY: {shipDetails.manufacturer}</h3>
+      <h3>HYPERDRIVE RATING: {shipDetails.hyperdrive_rating}</h3>
+      <Link to='/ships'><button>BACK</button></Link>
+    </div>
+
   )
 }
 
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,8 +1,17 @@
 const BASE_URL = 'https://swapi.dev/api/starships'
 
-export async function getStarships () {
+export async function getStarships() {
   try {
     const res = await fetch(BASE_URL)
+    return res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function getShipData(shipId) {
+  try {
+    const res = await fetch(`${BASE_URL}/${shipId}`)
     return res.json()
   } catch (err) {
     console.log(err)
