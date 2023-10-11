e5e1f527e4186cdfc10f31df222889396a8d6218
Ben Manley <benjamintmanley@gmail.com>
2023-10-11 00:51:35 -0500

    adjust for new API BASE_URL

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import { useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 
 import ShipList from './pages/ShipList/ShipList'
-import ShipDetailsWithLocationHook from './pages/ShipDetailsWithLocation/ShipDetailsWithLocation'
-import ShipDetailsWithoutLocationHook from './pages/ShipDetailsWithParams/ShipDetailsWithParams'
+import ShipDetailsWithLocation from './pages/ShipDetailsWithLocation/ShipDetailsWithLocation'
+import ShipDetailsWithParams from './pages/ShipDetailsWithParams/ShipDetailsWithParams'
 import Nav from './components/Nav/Nav'
 
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Nav />
       <Routes>
         <Route path='/ships' element={<ShipList />}  />
-        <Route path='/ships/:shipId' element={<ShipDetailsWithoutLocationHook />}  />
-        <Route path='/ships/details' element={<ShipDetailsWithLocationHook />}  />
+        <Route path='/ships/:shipId' element={<ShipDetailsWithParams />}  />
+        <Route path='/ships/details' element={<ShipDetailsWithLocation />}  />
       </Routes>
     </>
   )
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'https://swapi.dev/api/starships'
+const BASE_URL = 'https://sei-sw-api-a75665a714e3.herokuapp.com'
 
 export async function getStarships(page) {
   try {
-    const res = await fetch(`${BASE_URL}?page=${page}`)
+    const res = await fetch(`${BASE_URL}/api/starships?page=${page}`)
     return res.json()
   } catch (err) {
     console.log(err)
@@ -11,7 +11,7 @@ export async function getStarships(page) {
 
 export async function getShipData(shipId) {
   try {
-    const res = await fetch(`${BASE_URL}/${shipId}`)
+    const res = await fetch(`${BASE_URL}/api/starships/${shipId}`)
     return res.json()
   } catch (err) {
     console.log(err)
@@ -20,7 +20,7 @@ export async function getShipData(shipId) {
 
 export async function getPilots(pilotsArray) {
   try {
-    const promises = pilotsArray.map(pilotUrl => fetch(pilotUrl).then(res => res.json()))
+    const promises = pilotsArray.map(pilotUrl => fetch(`${BASE_URL}${pilotUrl}`).then(res => res.json()))
     const pilotData = await Promise.all(promises)
     return pilotData
   } catch (err) {
